8637912fec72d6a0f0a6a103a71a06e33ba624bd
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-09-03 00:24:19 +0530

    Update 5 540. Single Element in a Sorted Array.cpp

--- CN AMAZON SDE CHALLENGE/5 540. Single Element in a Sorted Array.cpp
+++ CN AMAZON SDE CHALLENGE/5 540. Single Element in a Sorted Array.cpp
@@ -1,6 +1,14 @@
 // https://leetcode.com/problems/single-element-in-a-sorted-array/
-//https://www.codingninjas.com/codestudio/problems/unique-element-in-sorted-array_1112654?topList=amazon-sde-challenge&leftPanelTab=0
+// https://www.codingninjas.com/codestudio/problems/unique-element-in-sorted-array_1112654?topList=amazon-sde-challenge&leftPanelTab=0
 
+
+// Hint
+// Use the fact that the array is sorted and for each element of the array, check if it is Shizuka’s lucky number.
+// Think of a solution using hashMap.
+// Think of a solution using bit manipulation.
+// Think of a solution using Binary search.
+
+    
 class Solution {
 public:
     int uniqueElement(vector<int> A, int n){
