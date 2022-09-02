7555121ea5338183387d177ae56d6bde2afb390c
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-09-02 07:04:48 +0530

    Create Minimum Value.cpp

---
+++ Minimum Value.cpp
@@ -0,0 +1,21 @@
+// https://www.codingninjas.com/codestudio/problems/minimum-value_975388?topList=amazon-sde-challenge&leftPanelTab=0
+
+#include<bits/stdc++.h>
+int minValue(vector<int>&arr, int n, int k)
+{
+    // Write your code here.
+    int diff,ans=INT_MAX;
+    sort(arr.begin(),arr.end());
+    int L=0,R=k-1;
+    while(R < n){
+        diff=arr[R]-arr[L];
+        if(diff<ans){
+            ans=diff;
+        }
+        L++,
+        R++;
+    }
+    return ans;
+}
+
+
