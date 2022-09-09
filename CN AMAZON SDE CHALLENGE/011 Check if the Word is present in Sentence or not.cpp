113440cff574bf8ef0a46b3206874aab034c9490
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-09-09 17:01:38 +0530

    Update 011 Check if the Word is present in Sentence or not.cpp

--- CN AMAZON SDE CHALLENGE/011 Check if the Word is present in Sentence or not.cpp
+++ CN AMAZON SDE CHALLENGE/011 Check if the Word is present in Sentence or not.cpp
@@ -20,3 +20,53 @@ bool findWord(string &s, string &w)
     return 0;
 }
 
+
+// APPROACH FROM SOLUTI0N BRUTE FORCE
+
+bool findWord(string &s, string w){
+   int n = s.size();
+   for (int i = 0; i < n; i++) {
+	 // To store the current word.
+	 string temp = "";
+	   
+	 // Processing the string to find individual words.
+	 while (i < n && s[i] != ' '){
+		temp += s[i];
+		i++;
+	 }
+	   
+	 // Checking if the current word is equal to W or not.
+	 if (temp == w){
+	 	return true;
+	 }
+    }
+    // If no word matches, then return false.
+    return false;
+}
+
+//     Time Complexity: O(|S|)
+//     Space Complexity: O(|S|)     where |S| is the length of the input sentence.
+
+
+// APPROACH FROM SOLUTI0N String processing using libraries
+
+#include <sstream>
+bool findWord(string &s, string &w){
+    // To store the current word.
+    string temp;
+
+    // Creating string stream
+    stringstream ss(s);
+
+    while (ss >> temp) {
+         // Comparing the current word temp with W
+	 if (temp == w) {
+		return true;
+         }
+    }  
+    return false;
+}
+
+
+//     Time Complexity: O(|S|)
+//     Space Complexity: O(|S|)     where |S| is the length of the input sentence.
