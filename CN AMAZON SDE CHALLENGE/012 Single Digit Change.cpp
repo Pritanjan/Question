6ec4cf9c4c486cc2cb6e9f2465cc4001cf0ab30a
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-09-19 13:38:53 +0530

    Create 012 Single Digit Change.cpp

---
+++ CN AMAZON SDE CHALLENGE/012 Single Digit Change.cpp
@@ -0,0 +1,16 @@
+// https://www.codingninjas.com/codestudio/problems/single-digit-change_1463972?topList=amazon-sde-challenge&leftPanelTab=0
+
+#include <bits/stdc++.h> 
+int findMaxNum(int num) {
+    string s = to_string(num);
+    for(int i=0; i<s.size(); i++){
+        if(s[i] == '6'){
+            s[i] = '9';
+            num = stoi(s);
+            break;
+        }
+    }
+    return num;
+}
+
+
