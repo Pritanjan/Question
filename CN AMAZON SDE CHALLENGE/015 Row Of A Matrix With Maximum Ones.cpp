5436d5d393ef6f9fe727a861119ec992f827a215
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-10-25 19:05:17 +0530

    Update 015 Row Of A Matrix With Maximum Ones.cpp

--- CN AMAZON SDE CHALLENGE/015 Row Of A Matrix With Maximum Ones.cpp
+++ CN AMAZON SDE CHALLENGE/015 Row Of A Matrix With Maximum Ones.cpp
@@ -18,4 +18,25 @@ int ninjaAndLadoos(vector<int> &r1, vector<int> &r2, int m, int n, int k) {
 
 
 
+// TLE
+
+
+
+int ninjaAndLadoos(vector<int> &arr1, vector<int> &arr2, int m, int n, int k){
+    int ans[m + n];
+    int i = 0, j = 0, d = 0;
+    while (i < m && j < n)
+    {
+        if (arr1[i] < arr2[j])
+            ans[d++] = arr1[i++];
+        else
+            ans[d++] = arr2[j++];
+    }
+    while (i < m)
+        ans[d++] = arr1[i++];
+    while (j < n)
+        ans[d++] = arr2[j++];
+    return ans[k - 1];
+}
+
 
