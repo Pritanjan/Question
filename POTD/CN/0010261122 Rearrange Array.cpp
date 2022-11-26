1ad2c6abd9e70701f6c36b30e013963984281202
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-11-26 15:43:49 +0530

    Create 0010261122 Rearrange Array.cpp

---
+++ POTD/CN/0010261122 Rearrange Array.cpp
@@ -0,0 +1,20 @@
+// https://www.codingninjas.com/codestudio/problem-of-the-day/easy?leftPanelTab=0
+
+
+#include <bits/stdc++.h> 
+void rearrangeArray(int arr[], int n) {
+//     vector<int> v;
+//     int v[n];
+	for(int i=0; i<n; i++){
+        arr[i] += (arr[arr[i]] % n) * n;
+    }
+    
+    for (int i = 0; i < n; i++)
+        arr[i] /= n;
+    
+    return ;
+}
+
+
+
+
