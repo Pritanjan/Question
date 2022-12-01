d921c26a7865010c6cf68933149e97623c3491b4
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-12-02 01:22:53 +0530

    Update 0024 021222 Check if it is possible to convert one string into another with given constraints.cpp

--- POTD/gfg/0024 021222 Check if it is possible to convert one string into another with given constraints.cpp
+++ POTD/gfg/0024 021222 Check if it is possible to convert one string into another with given constraints.cpp
@@ -1,5 +1,7 @@
 // https://practice.geeksforgeeks.org/problems/check-if-it-is-possible-to-convert-one-string-into-another-with-given-constraints4116/1
 
+// APPROACH 1
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -36,5 +38,38 @@ int main() {
 
 
 
+// APPROACH 2
 
+// If the sizes of the two strings are not the same, then the strings can never be the same.
+
+// If the frequency of A and B in the two strings are not the same, then the strings can never
+// be the same.
+
+// Back-end complete function Template for C++
+class Solution {
+  public:
+    int isItPossible(string S, string T, int M, int N) {
+        int i=0, j=0;
+        if(M!=N) return 0;
+        
+        while(i<M || j<N){
+            while(S[i]=='#') i++;
+            while(T[j]=='#') j++;
+            
+            if(i>=M && j<N) return 0;
+            if(i<M && j>=N) return 0;
+            
+            if(S[i] != T[j]) return 0;
+            else{
+                if(S[i]=='A' && i<j) return 0;
+                else if(S[i]=='B' && i>j) return 0;
+            }
+            i++;
+            j++;
+        }        
+        return 1;
+    }
+};
+
+    
 
