67b731e94e167d03c57ff04c92f0549cb768717c
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-12-17 16:10:12 +0530

    Create 0028 281222 Break a number into 3 parts.cpp

---
+++ POTD/gfg/0028 281222 Break a number into 3 parts.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{   
+public:
+    int waysToBreakNumber(int n){
+        long mod = (1e9 + 7);
+        return ((long)(n + 1) * (long)(n + 2) / 2) % mod;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.waysToBreakNumber(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
+
+
+
+
+
+
+
