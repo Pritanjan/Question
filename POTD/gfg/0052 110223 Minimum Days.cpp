bb7a01c03917dccd6923966637a3be1df40d30fb
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-02-11 12:48:35 +0530

    Update 0052 110223 Minimum Days.cpp

--- POTD/gfg/0052 110223 Minimum Days.cpp
+++ POTD/gfg/0052 110223 Minimum Days.cpp
@@ -1,5 +1,7 @@
 // https://practice.geeksforgeeks.org/problems/f4d22b1f9d62e8bee0ff84e9fa51dc66eb5005ec/1
 
+// APPROAH 1
+
 //{ Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
@@ -78,3 +80,91 @@ int main(){
 }
 
 // } Driver Code Ends
+
+
+
+
+
+
+
+
+// APPROAH 2
+
+class Solution {
+  public:
+    int getMinimumDays(int N,string S, vector<int> &P) {
+        int count = 0;
+        for(int i=0; i<N; i++) {
+            char c = S[P[i]];
+            if((P[i] == 0 && S[P[i]+1] == c) || (P[i] == N-1 && S[P[i]-1] == c) || ((S[P[i]-1] == c) || (S[P[i]+1] == c))) {
+                S[P[i]] = '?';
+                count = i+1;
+            }
+        }
+        return count;
+    }
+};
+
+
+
+
+// APPROAH 3
+
+A Binary Search Solution!!
+
+bool ispossible(int mid, string s, vector<int> &p)
+
+    {
+
+        for(int i = 0; i < mid; i++)
+
+        {
+
+            s[p[i]] = '?';
+
+        }
+
+        for(int i = 0; i < s.length()-1; i++)
+
+        {
+
+            if(s[i] == s[i+1] && s[i] != '?') return 0;
+
+        }
+
+        return true;
+
+    }
+
+  public:
+
+    int getMinimumDays(int N,string s, vector<int> &p) {
+
+        int l = 0, r = N;
+
+        int ans = N;
+
+        while(l <= r)
+
+        {
+
+            int mid = l - (l - r) / 2;
+
+            if(ispossible(mid, s, p)){
+
+                ans = mid;
+
+                r = mid - 1;
+
+            }
+
+            else{
+
+                l = mid + 1;
+
+            }
+
+        }
+
+       return ans;
+
