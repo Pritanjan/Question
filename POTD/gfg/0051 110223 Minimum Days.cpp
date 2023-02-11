07e978e84c97116bfa719cad35310aa081134878
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-02-11 12:37:58 +0530

    Create 0051 110223 Minimum Days.cpp

---
+++ POTD/gfg/0051 110223 Minimum Days.cpp
@@ -0,0 +1,80 @@
+// https://practice.geeksforgeeks.org/problems/f4d22b1f9d62e8bee0ff84e9fa51dc66eb5005ec/1
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int getMinimumDays(int N,string S, vector<int> &P) {
+        int ans = 0;
+        int i = 0;
+        
+        while(i < N-1){
+            if(S[i] != '?') {
+                if(S[i] != S[i+1]) {
+                    i++;
+                    continue;
+                }
+                else {
+                    S[P[ans]]='?';
+                    ans++;
+                }
+            }
+            else
+                i++;
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        string S;cin>>S;
+        
+        vector<int> P(N);
+        Array::input(P,N);
+        
+        Solution obj;
+        int res = obj.getMinimumDays(N,S, P);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
