7a477b06c0fa8ef5205ceb70b36997ef06927b58
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-12 03:33:34 +0530

    Create 0067 120323 Binary matrix having maximum number of 1s.cpp

---
+++ POTD/gfg/0067 120323 Binary matrix having maximum number of 1s.cpp
@@ -0,0 +1,25 @@
+// https://practice.geeksforgeeks.org/problems/77e1c3e12cd148f835d53eb168d4472b2ff539fa/1
+
+class Solution {
+public:
+    vector<int> findMaxRow(vector<vector<int>> mat, int N) {
+        int cnt = 0;
+        int row = 0;
+        
+        for(int i=0; i<N; i++){
+            vector<int>::iterator upper;
+            upper = upper_bound(mat[i].begin(), mat[i].end(), 0) ;
+            int temp =  N - (upper - mat[i].begin());
+             
+            if(temp > cnt){
+                cnt = temp;
+                row = i;
+            }
+        }
+        
+        return {row , cnt}; 
+    }
+};
+
+
+
