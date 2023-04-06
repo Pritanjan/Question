5c70681df0226d6686965936721644471392b023
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-07 00:49:01 +0530

    Create 0085 070423 Add Minimum Characters.cpp

---
+++ POTD/gfg/0085 070423 Add Minimum Characters.cpp
@@ -0,0 +1,51 @@
+// https://practice.geeksforgeeks.org/problems/55dbfdc246f3f62d6a7bcee7664cacf6be345527/1
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    int addMinChar(string str){    
+        int n = str.size();
+        int i = 0, j = n-1, ans = n-1;
+        
+        while(i < j) {
+            if(str[i] == str[j]) {
+                i++;
+                j--;
+            }
+            else {
+                i = 0;
+                ans--;
+                j = ans;
+            }
+        }
+        return n-ans-1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string str;
+        cin >> str;
+        
+        Solution ob;
+        cout << ob.addMinChar(str) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
