0fa5b10df1f540f6b7890e57ae95231635805156
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-10 10:29:52 +0530

    Update 0087 100423 Maximum Intersecting Lines.cpp

--- POTD/gfg/0087 100423 Maximum Intersecting Lines.cpp
+++ POTD/gfg/0087 100423 Maximum Intersecting Lines.cpp
@@ -1,5 +1,8 @@
 // https://practice.geeksforgeeks.org/problems/63c232252d445a377e01cd91adfd7d1060580038/1
 
+
+// APPROACH 1
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -34,6 +37,33 @@ int main() {
     }
 }
 
+
+
+
+
+
+
+// APPROACH 2
+
+class Solution {
+public:
+    int maxIntersections(vector<vector<int>> lines, int N) {
+        map<int, int> map;
+        for(vector<int> arr : lines) {
+            map[arr[0]] = map[arr[0]] + 1;
+            map[arr[1]+1] = map[arr[1]+1] - 1;
+        }
+        
+        int ans = -1;
+        int prefixSum = 0;
+        for(auto const &i : map) {
+            prefixSum += i.second;
+            ans = max(ans, prefixSum);
+        }
+        
+        return ans;
+    }
+};
 
 
 
