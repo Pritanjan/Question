7c8e7e260e627c4de744750ed01bc20af64dec05
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-14 02:59:11 +0530

    Update 0090 140423 Remove the balls.cpp

--- POTD/gfg/0090 140423 Remove the balls.cpp
+++ POTD/gfg/0090 140423 Remove the balls.cpp
@@ -18,3 +18,23 @@ class Solution {
 
 
 
+
+
+
+
+class Solution {
+  public:
+    int finLength(int N, vector<int> color, vector<int> radius) {
+        stack<pair<int,int>>st;
+        st.push({color[0],radius[0]});
+        for(int i=1;i<N;i++) {
+            if(!st.empty() and st.top().first==color[i] and st.top().second==radius[i]) st.pop();
+            else st.push({color[i],radius[i]});
+        }
+        return st.size();
+    }
+};
+
+
+
+
