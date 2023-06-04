202e037e2c895ac45f2f8cd2b5c51058661ed8b0
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-06-04 07:30:39 +0530

    Create 0105 040623 Reversing the equation.cpp

---
+++ POTD/gfg/0105 040623 Reversing the equation.cpp
@@ -0,0 +1,21 @@
+// https://practice.geeksforgeeks.org/problems/reversing-the-equation2205/1
+
+
+class Solution {
+public:
+    string reverseEqn (string s) {
+        reverse(s.begin(), s.end());
+        int n = s.size();
+        for(int i=0; i<n; i++) {
+            if(s[i] >= '0' && s[i] <= '9') {
+                int j = i;
+                while(j < n && (s[j] >= '0' && s[j] <= '9')) j++;
+                reverse(s.begin() + i, s.begin() + j);
+                i = j;
+            }
+        }
+        return s;
+    }
+};
+
+
