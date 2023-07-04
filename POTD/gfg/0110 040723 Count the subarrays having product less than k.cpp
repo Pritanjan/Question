7a7f7ef55dc9bc2faff71ee295930d0b5e98f822
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-07-04 18:48:36 +0530

    Create 0110 040723 Count the subarrays having product less than k.cpp

---
+++ POTD/gfg/0110 040723 Count the subarrays having product less than k.cpp
@@ -0,0 +1,42 @@
+// https://practice.geeksforgeeks.org/problems/count-the-subarrays-having-product-less-than-k1708/1
+
+// LC 713
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    int countSubArrayProductLessThanK(const vector<int>& a, int n, long long k) {
+        long p = 1;
+        int count = 0;
+        for(int i=0; i<n; i++) {
+            if(a[i] < k) count++;
+            p = a[i];
+            for(int j=i+1; j<n; j++) {
+                p *= a[j];
+                if(p < k) count++;
+                else break;
+            }
+        }
+        return count;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        long long int k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        for (i = 0; i < n; i++) cin >> arr[i];
+        Solution obj;
+        cout << obj.countSubArrayProductLessThanK(arr, n, k) << endl;
+    }
+    return 0;
+}
+
+
