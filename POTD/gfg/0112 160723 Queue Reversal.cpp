8ef0b9146fa068df01f6fe2c412e0404afa3b672
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-07-16 13:10:50 +0530

    Update 0112 160723 Queue Reversal.cpp

--- POTD/gfg/0112 160723 Queue Reversal.cpp
+++ POTD/gfg/0112 160723 Queue Reversal.cpp
@@ -1,7 +1,7 @@
 // APPROACH 1 USING STACK
 // element ko queue se nikal ko stack me dalo
 
-class Solution{
+class Solution {
 public:
     queue<int> rev(queue<int> que) {
         stack<int> stk;
@@ -19,11 +19,41 @@ public:
 };
 
 
+// T.C.  -->  O(N)
+// S.C.  -->  O(N)
+
+
+
 
 
 
 
 // APPROACH 2 RECURSION
+
+class Solution {
+public:
+     void reverseQueue(queue<int>& q) {
+        // base case
+        if (q.size() == 0) return ;
+        
+        int fr = q.front();
+        q.pop();
+        
+        reverseQueue(q);
+        q.push(fr);
+    }
+
+    queue<int> rev(queue<int> q) {
+        reverseQueue(q);
+        return q;
+    }
+};
+
+
+// T.C.  -->  O(N^2)
+// S.C.  -->  O(N)
+
+
 
 
 
